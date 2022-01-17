d581523f35a4f16b9f6ed9b15d349fb7e089a9ed
Polina Bobrova <bobrrrovapolina@gmail.com>
2022-01-17 23:51:57 +0300

    Add 1

--- Hello.java
+++ Hello.java
@@ -2,6 +2,6 @@ class HelloWorld {
     public static void main(String[] args) {
         System.out.println("A");
         System.out.println("B");
-        System.out.println("Hello World!");
+        System.out.println("Hello World!1");
     }
 }
